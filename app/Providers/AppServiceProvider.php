344a54a3537e1668b292bdbd88e3d7067aa7e35c
daiki-kudou <67718513+daiki-kudou@users.noreply.github.com>
2020-10-22 18:58:04 +0900

    force scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,25 +6,28 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    //
+  }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        // 追加
-        \Schema::defaultStringLength(191);
-        // \URL::forceScheme('https');
-    }
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    // 追加
+    \Schema::defaultStringLength(191);
+    \URL::forceScheme('https');
+    // if (request()->isSecure()) {
+    //   \URL::forceScheme('https');
+    // }
+  }
 }
