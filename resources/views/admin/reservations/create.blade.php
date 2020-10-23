4d3a9c337f27e131e4560f75a6a02b039ad57b1b
daiki-kudou <67718513+daiki-kudou@users.noreply.github.com>
2020-10-23 18:09:13 +0900

    reservations createのテンプレ修正

--- resources/views/admin/reservations/create.blade.php
+++ resources/views/admin/reservations/create.blade.php
@@ -81,6 +81,373 @@
 
 
 
+
+
+
+
+
+
+
+<div class="container-field mt-3">
+  <div class="float-right">
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item active">{{ Breadcrumbs::render(Route::currentRouteName()) }}</li>
+      </ol>
+    </nav>
+  </div>
+  <h1 class="mt-3 mb-5">予約　新規登録</h1>
+  <hr>
+</div>
+
+<div class="container-field bg-white text-dark">
+  <div class="border border border-success">
+    <table class="table mb-0">
+      <tr>
+        <td class="">予約概要 ID : 00010</td>
+        <td class="">編集</td>
+      </tr>
+    </table>
+  </div>
+
+  <div class="mt-2">
+    <div class="d-flex ml-2">
+      <table class="table mr-2 border border-light col-10">
+        <tr>
+          <td class="bg-success text-white">予約状況</td>
+          <td>予約確認中</td>
+          <td class="bg-success text-white">チェック</td>
+          <td>未</td>
+          <td>
+            <div class="btn btn-primary">チェックする</div>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <table class="table table-bordered">
+        <tr>
+          <td colspan="2">予約情報</td>
+        </tr>
+        <tr>
+          <td class="table-active">利用日</td>
+          <td>2020/02/12</td>
+        </tr>
+        <tr>
+          <td class="table-active">会場</td>
+          <td>四ツ橋サンワールドビル</td>
+        </tr>
+        <tr>
+          <td class="table-active">入室時間</td>
+          <td>09:00</td>
+        </tr>
+        <tr>
+          <td class="table-active">退室時間</td>
+          <td>19:00</td>
+        </tr>
+        <tr>
+          <td>案内板</td>
+          <td>要作成</td>
+        </tr>
+        <tr>
+          <td class="table-active">イベント開始時間</td>
+          <td>10:00</td>
+        </tr>
+        <tr>
+          <td class="table-active">イベント終了時間</td>
+          <td>17:00</td>
+        </tr>
+        <tr>
+          <td class="table-active">イベント名称1</td>
+          <td>会社説明会</td>
+        </tr>
+        <tr>
+          <td class="table-active">イベント名称2</td>
+          <td>2021年新卒採用</td>
+        </tr>
+        <tr>
+          <td class="table-active">主催者s名</td>
+          <td>株式会社BAC</td>
+        </tr>
+        <tr>
+          <td>有料備品</td>
+        </tr>
+      </table>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="col">col2</div>
+  </div>
+</div>
+
+{{-- <div class="container-field">
+  {{ Form::open(['url' => 'admin/reservations', 'method'=>'POST', 'id'=>'']) }}
+@csrf
+
+<div class="row">
+  <div class="col">
+    <div class="p-3 mb-2 bg-white text-dark">
+
+      <table class="table table-bordered">
+        <tr>
+          <td>予約情報</td>
+        </tr>
+        <tr>
+          <td>利用日</td>
+          <td>2020/10/20(火)</td>
+        </tr>
+      </table>
+
+      <div class="row">
+        <div class="col-sm-4">{{ Form::label('remark', '備考') }}</div>
+        <div class="col-sm-8">
+          {{ Form::textarea('remark', old('remark'), ['class' => 'form-control']) }}</div>
+      </div>
+    </div>
+
+    <div class="p-3 mb-2 bg-white text-dark">
+      <span><i class="fas fa-suitcase-rolling"></i>荷物預かり</span>
+
+      <div class="row">
+        <div class="col-sm-4">{{ Form::label('luggage_flag', '荷物預かり　有・無') }}</div>
+        <div class="col-sm-8">
+          {{Form::select('luggage_flag', ['有り', '無し'],null,['placeholder' => '選択してください','class'=>'custom-select mr-sm-2'])}}
+        </div>
+      </div>
+      <hr>
+
+      <div class="row">
+        <div class="col-sm-4">{{ Form::label('luggage_post_code', '送付先郵便番号') }}</div>
+        <div class="col-sm-8"> {{ Form::text('luggage_post_code', old('luggage_post_code'), [
+            'class' => 'form-control',
+            'onKeyUp'=>"AjaxZip3.zip2addr(this,'','luggage_address1','luggage_address2');",
+            'autocomplete'=>'off',
+            ]) }}
+        </div>
+      </div>
+      <hr>
+
+      <div class="row">
+        <div class="col-sm-4">{{ Form::label('luggage_address1', '住所（都道府県）') }}</div>
+        <div class="col-sm-8">
+          {{ Form::text('luggage_address1', old('luggage_address1'), ['class' => 'form-control']) }}</div>
+      </div>
+      <hr>
+
+      <div class="row">
+        <div class="col-sm-4">{{ Form::label('luggage_address2', '住所（市町村番地）') }}</div>
+        <div class="col-sm-8">
+          {{ Form::text('luggage_address2', old('luggage_address2'), ['class' => 'form-control']) }}</div>
+      </div>
+      <hr>
+
+      <div class="row">
+        <div class="col-sm-4">{{ Form::label('luggage_address3', '住所（建物名）') }}</div>
+        <div class="col-sm-8">
+          {{ Form::text('luggage_address3', old('luggage_address3'), ['class' => 'form-control']) }}</div>
+      </div>
+      <hr>
+
+      <div class="row">
+        <div class="col-sm-4">{{ Form::label('luggage_name', '送付先名') }}</div>
+        <div class="col-sm-8">
+          {{ Form::text('luggage_name', old('luggage_name'), ['class' => 'form-control']) }}</div>
+      </div>
+      <hr>
+
+      <div class="row">
+        <div class="col-sm-4">{{ Form::label('luggage_tel', '電話番号') }}</div>
+        <div class="col-sm-8">
+          {{ Form::text('luggage_tel', old('luggage_tel'), ['class' => 'form-control']) }}</div>
+      </div>
+
+    </div>
+
+  </div>
+  <div class="col">
+    <div class="p-3 mb-2 bg-white text-dark">
+      <span><i class="fas fa-user-check"></i>担当者情報</span>
+
+      <div class="row">
+        <div class="col-sm-2">{{ Form::label('first_name', '氏名(姓)') }}</div>
+        <div class="col-sm-4">
+          {{ Form::text('first_name', old('first_name'), ['class' => 'form-control']) }}</div>
+        <div class="col-sm-2">{{ Form::label('last_name', '氏名(名)') }}</div>
+        <div class="col-sm-4">
+          {{ Form::text('last_name', old('last_name'), ['class' => 'form-control']) }}</div>
+      </div>
+      <hr>
+
+      <div class="row">
+        <div class="col-sm-2">{{ Form::label('first_name', '氏名(セイ)') }}</div>
+        <div class="col-sm-4">
+          {{ Form::text('first_name_kana', old('first_name_kana'), ['class' => 'form-control']) }}</div>
+        <div class="col-sm-2">{{ Form::label('last_name', '氏名(メイ)') }}</div>
+        <div class="col-sm-4">
+          {{ Form::text('last_name_kana', old('last_name_kana'), ['class' => 'form-control']) }}</div>
+      </div>
+      <hr>
+
+      <div class="row">
+        <div class="col-sm-4">{{ Form::label('person_tel', '担当者電話番号') }}</div>
+        <div class="col-sm-8">
+          {{ Form::text('person_tel', old('person_tel'), ['class' => 'form-control']) }}</div>
+      </div>
+      <hr>
+
+      <div class="row">
+        <div class="col-sm-4">{{ Form::label('person_email', '担当者メール') }}</div>
+        <div class="col-sm-8">
+          {{ Form::text('person_email', old('person_email'), ['class' => 'form-control']) }}</div>
+      </div>
+    </div>
+
+    <div class="p-3 mb-2 bg-white text-dark">
+      <span><i class="fas fa-building"></i>ビル管理会社</span>
+
+      <div class="row">
+        <div class="col-sm-4">{{ Form::label('mgmt_company', '会社名') }}</div>
+        <div class="col-sm-8">
+          {{ Form::text('mgmt_company', old('mgmt_company'), ['class' => 'form-control']) }}</div>
+      </div>
+      <hr>
+
+      <div class="row">
+        <div class="col-sm-4">{{ Form::label('mgmt_tel', '電話番号') }}</div>
+        <div class="col-sm-8">
+          {{ Form::text('mgmt_tel', old('mgmt_tel'), ['class' => 'form-control']) }}</div>
+      </div>
+      <hr>
+
+      <div class="row">
+        <div class="col-sm-4">{{ Form::label('mgmt_emer_tel', '夜間緊急連絡先') }}</div>
+        <div class="col-sm-8">
+          {{ Form::text('mgmt_emer_tel', old('mgmt_emer_tel'), ['class' => 'form-control']) }}</div>
+      </div>
+      <hr>
+
+      <div class="row">
+        <div class="col-sm-2">{{ Form::label('mgmt_first_name', '氏名(姓)') }}</div>
+        <div class="col-sm-4">
+          {{ Form::text('mgmt_first_name', old('mgmt_first_name'), ['class' => 'form-control']) }}</div>
+        <div class="col-sm-2">{{ Form::label('mgmt_last_name', '氏名(名)') }}</div>
+        <div class="col-sm-4">
+          {{ Form::text('mgmt_last_name', old('mgmt_last_name'), ['class' => 'form-control']) }}</div>
+      </div>
+      <hr>
+
+      <div class="row">
+        <div class="col-sm-4">{{ Form::label('mgmt_email', '担当者メール') }}</div>
+        <div class="col-sm-8">
+          {{ Form::text('mgmt_email', old('mgmt_email'), ['class' => 'form-control']) }}</div>
+      </div>
+      <hr>
+
+      <div class="row">
+        <div class="col-sm-4">{{ Form::label('mgmt_sec_company', '警備会社名') }}</div>
+        <div class="col-sm-8">
+          {{ Form::text('mgmt_sec_company', old('mgmt_sec_company'), ['class' => 'form-control']) }}</div>
+      </div>
+      <hr>
+
+      <div class="row">
+        <div class="col-sm-4">{{ Form::label('mgmt_sec_tel', '警備会社電話番号') }}</div>
+        <div class="col-sm-8">
+          {{ Form::text('mgmt_sec_company', old('mgmt_sec_company'), ['class' => 'form-control']) }}</div>
+      </div>
+      <hr>
+
+      <div class="row">
+        <div class="col-sm-4">{{ Form::label('mgmt_remark', '備考') }}</div>
+        <div class="col-sm-8">
+          {{ Form::textarea('mgmt_remark', old('mgmt_remark'), ['class' => 'form-control']) }}</div>
+      </div>
+      <hr>
+    </div>
+
+
+
+
+    <div class="p-3 mb-2 bg-white text-dark">
+      <span><i class="fas fa-utensils"></i>室内飲食</span>
+
+      <div class="row">
+        <div class="col-sm-4">{{ Form::label('eat_in_flag', '室内飲食') }}</div>
+        <div class="col-sm-8">
+          {{{Form::select('eat_in_flag', ['有り', '無し'],null,['placeholder' => '選択してください', 'class'=>'custom-select mr-sm-2'])}}}
+        </div>
+      </div>
+      <hr>
+    </div>
+
+    <div class="p-3 mb-2 bg-white text-dark">
+      <span><i class="fas fa-utensils"></i>支払データ</span>
+
+      <div class="row">
+        <div class="col-sm-4">{{ Form::label('cost', '支払割合（原価）') }}</div>
+        <div class="col-sm-8">
+          {{ Form::number('cost', old('cost'), ['class' => 'form-control']) }}</div>
+      </div>
+    </div>
+    <hr>
+  </div>
+</div>
+</div>
+
+
+
+<div class="p-3 mb-2 bg-white text-dark">
+  <span>有料備品</span>
+  <div>
+    <span>※左部リストよりクリックで選択し右部リストに移動させてください</span>
+  </div>
+  <select id='equipment_id' multiple='multiple' name="equipment_id[]">
+  </select>
+</div>
+
+<div class="p-3 mb-2 bg-white text-dark">
+  <span>有料サービス</span>
+  <div>
+    <span>※左部リストよりクリックで選択し右部リストに移動させてください</span>
+  </div>
+  <select id='service_id' multiple='multiple' name="service_id[]">
+  </select>
+</div>
+
+<div class="mx-auto" style="width: 100px;">
+  {{ Form::submit('登録', ['class' => 'btn btn-primary']) }}
+</div>
+
+
+{{ Form::close() }}
+</div>
+--}}
+
+
+
+
+
+
 {{-- 
 
 <div class="container-field mt-3">
